3e4f4b37041b043144482b0eb66381aebc0eedc8
hoangdo <shinkenger.vn9x@gmail.com>
2018-10-02 03:46:19 +0000

    Revert "fetch xemvn"
    This reverts commit 2f826041233f0f374afc0cab7f9dbd86366e048c

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,4 @@
 import {Request, Response, Router} from "express";
-import Axios from "axios";
-import {JSDOM} from "jsdom";
 
 export class IndexRouter {
     public router: Router;
@@ -11,32 +9,8 @@ export class IndexRouter {
     }
 
     routing() {
-        this.router.get("/", async (req: Request, res: Response) => {
-            const response: any[] = [];
-            for (let i=9; i>=0; i--) {
-                let promises = [];
-                for (let j = 10; j>0; j--) {
-                    promises.push(Axios.get(`https://xem.vn/new/${i*10 + j}`));
-                }
-
-                for (let p of promises) {
-                    try {
-                        const dom = new JSDOM((await p).data);
-                        let blocks = dom.window.document.querySelectorAll('.photoListItem > .thumbnail');
-                        for (let b of blocks) {
-                            let b1: Element = b;
-                            response.push({
-                                img: b1.querySelector('img').src,
-                                title: b1.querySelector('img').alt
-                            })
-                        }
-                    } catch (e) {
-                        console.log(e.message);
-                    }
-                }
-            }
-
-            res.send(response);
+        this.router.get("/", function(req: Request, res: Response) {
+            res.render("index", { title: "Express" });
         });
     }
 }
